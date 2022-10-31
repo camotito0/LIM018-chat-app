292c963f071863dfa80ac0044df0a547dbf8e4f5
camotito0 <cecilianallerena@gmail.com>
2022-10-31 15:15:44 -0500

    Adding functions generateToken and authenticationToken in file utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const encrypt = (password) => {
     return bcrypt.hashSync(password, 10)
@@ -9,7 +10,23 @@ const compare = (password, passEncrypt) => {
     return bcrypt.compareSync(password, passEncrypt);
 }
 
+const generateToken = (user) => {
+    return jwt.sign(user, 'shhhh', { expiresIn: '8h' })
+}
+
+const authenticationToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    jwt.verify(token, 'shhhh', (err, user) => {
+        if(err) return res.status(403).send({message:'Access denied, token expired or incorrect'})
+        req.user = user
+        next();
+    })
+}
+
 module.exports = {
     encrypt,
-    compare
+    compare,
+    generateToken,
+    authenticationToken
 }
