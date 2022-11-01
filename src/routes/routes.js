06f4760bcef1521498d101ec6e6e66075eedb39e
camotito0 <cecilianallerena@gmail.com>
2022-11-01 16:08:00 -0500

    Adding routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,7 @@ const router = require('express').Router();
 /* controllers */
 const auth = require('../controllers/auth.controller');
 const user = require('../controllers/user.controller');
+const channel = require('../controllers/channels.controller');
 
 /* middleware auth */
 const {authenticationToken} = require('../utils');
@@ -13,9 +14,14 @@ const {authenticationToken} = require('../utils');
 router.post('/auth/register', auth.registerUser);
 router.post('/auth/login', auth.loginUser);
 
-router.put('/users/:id', authenticationToken, user.updateUser)
-router.delete('/users/:id', authenticationToken, user.deleteUser)
-router.get('/users', authenticationToken, user.getUsers)
+router.put('/users/:id', authenticationToken, user.updateUser);
+router.delete('/users/:id', authenticationToken, user.deleteUser);
+router.get('/users', authenticationToken, user.getUsers);
 
+router.post('/channel', authenticationToken, channel.createChannel);
+router.put('/channels/:id', authenticationToken, channel.updateChannel);
+router.delete('/channels/:id', authenticationToken, channel.deleteChannel);
+router.get('/channelsByUser', authenticationToken, channel.getUserChannelsJoin);
+router.get('/channels', authenticationToken, channel.getAllChannels);
 
 module.exports = router;
